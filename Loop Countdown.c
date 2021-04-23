1caf21670a46120e59ce47c17521a7ea61213c34
Mark Kenneth Ferrer <81341766+Dev-Kenn@users.noreply.github.com>
2021-04-23 18:11:19 +0800

    Add files via upload

---
+++ Loop Countdown.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/*Mark Kenneth O. Ferrer DICT 1-3*/
+int main() {
+	int i, n;
+
+    printf("Countdown 100 to 0: \n", n);
+
+ 
+    for(i=100; i>=0; i--)
+    {
+       
+        if(i%1 == 0)
+        {
+            printf("%d\n", i);
+        }
+    }
+
+    return 0;
+}
